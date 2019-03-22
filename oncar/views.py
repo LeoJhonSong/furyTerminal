fcc63b211c4868cf3bb75c40d7a6af5c36f0aedf
Leo-Dell-Ubuntu <LeoJhon.Song@outlook.com>
2019-03-23 06:19:51 +0800

    完成speed_bar功能

--- oncar/views.py
+++ oncar/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 from .models import Speed
 
 
@@ -9,3 +11,10 @@ def index(request):
         'max_speed': 9
     }
     return render(request, 'oncar/index.html', context)
+
+def refresh(request):
+    data = {
+        'current_speed': Speed.objects.order_by('-speed_time')[0].speed_value,
+        'max_speed': 9
+    }
+    return JsonResponse(data)
