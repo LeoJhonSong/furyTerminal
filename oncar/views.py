58a219a56eec6cda0ed0dddf978a2101fbdd9b05
LeoJhonSong <LeoJhon.Song@outlook.com>
2019-10-11 06:42:24 +0800

    feat: rewrite template using DTL

--- oncar/views.py
+++ oncar/views.py
@@ -5,13 +5,16 @@ from .models import Speed
 
 max_speed = 80
 
+
 def index(request):
+    # FIXME: write to a file instead
     current_speed = Speed.objects.order_by('-speed_time')[0].speed_value
     context = {
         'current_speed': current_speed,
         'max_speed': max_speed
     }
     return render(request, 'oncar/index.html', context)
+
 
 def refresh(request):
     data = {
