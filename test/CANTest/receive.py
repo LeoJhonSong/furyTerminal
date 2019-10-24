4fe590a1f1289434be06c68fe609baa3abc655d5
Leo-RPi <LeoJhon.Song@outlook.com>
2019-10-25 01:06:30 +0800

    fix: use relative path so scripts become more robust. fix: ignore db.sqlite3

--- test/CANTest/receive.py
+++ test/CANTest/receive.py
@@ -6,27 +6,19 @@ import can
 os.system('sudo ip link set can0 type can bitrate 500000')
 os.system('sudo ifconfig can0 up')
 
-can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')# socketcan_native
+# socketcan_native
+can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan_ctypes')
 
-#msg = can.Message(arbitration_id=0x123, data=[0, 1, 2, 3, 4, 5, 6, 7], extended_id=False)
-##msg = can0.recv(10.0)
-##print(msg)
-i = 0x1
-idd = 0x121
 for msg in can0:
-    msgdata = ''.join(str(i) for i in msg.data)
-    print(msgdata +"\n")
+    msgdata = ','.join(str(i) for i in msg.data)
+    print(msgdata)
     print("id is " + str(msg.arbitration_id))
-    idd = idd + i
-    i = i + 0x1
-    
-    msgs = can.Message(arbitration_id=idd, data=msg.data)
-    can0.send(msgs)
-    
+
+    # DEBUG: send back
+    # myId = 0x121
+    # can0.send(can.Message(arbitration_id=myId, data=msg.data))
+
 if msg is  None:
     print('Timeout occurred, no message.')
-##else:
-##    msg = can.Message(arbitration_id=0x123, data=[3, 1, 4, 1, 5, 9, 2, 6], extended_id=False)
-##    can0.send(msg)
 
 os.system('sudo ifconfig can0 down')
