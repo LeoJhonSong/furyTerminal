180a1577fbda100155c601ad91969bec42e47b48
Leo-Dell-Ubuntu <LeoJhon.Song@outlook.com>
2019-11-15 12:51:53 +0800

    feat(database): change back database to sqlite
    as tested, sqlite ver could perform well on speed, despite multiprocess

--- furyTerminal/settings.py
+++ furyTerminal/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'furyTerminal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'furyTerminal',
-        'USER': 'fury',
-        'PASSWORD': 'fury',
-        'HOST': '',  # localhost
-        'PORT': '',  # 3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
