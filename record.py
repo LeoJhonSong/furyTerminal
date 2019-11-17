66f54c5d137a37d1866eb369cd47ad39b94d6820
Leo-Dell-Ubuntu <LeoJhon.Song@outlook.com>
2019-11-17 20:11:44 +0800

    fix(can bus read and server): kill can bus buffer, fix record crash on db-lock
    shutdown can instance after reading every time
    increase oncar web page refresh rate to 500ms

--- record.py
+++ record.py
@@ -6,6 +6,7 @@
 
 import os
 import time
+import datetime
 
 import furyCAN.bus
 
@@ -14,11 +15,18 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'furyTerminal.settings')
 django.setup()
 from oncar.models import State
 
-can1 = furyCAN.bus.CAN()
 while True:
-    id, data = can1.decode()
-    can1.read(id, data)
+    can1 = furyCAN.bus.CAN()
+    switch = 0
+    last = time.time()
+    # to collect all state data, read for a while
+    while time.time() - last < 0.2:
+        id, data = can1.decode()
+        can1.read(id, data)
+        switch = 1
     # **can1.state allow us to pass this dict as a argument list to the function
-    State.objects.create(**can1.state)
-    # refresh period (s)
-    time.sleep(0.05)
+    if switch:
+        State.objects.create(**can1.state)
+        print(datetime.datetime.now())
+        # State.objects.create(speed=datetime.datetime.now().second)
+    can1.bus.shutdown()
